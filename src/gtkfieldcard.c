43042b6d0ebbf1fe45122db82b4b4c4a52aa77f8
Pyr0x1 <gabrieleloris@gmail.com>
2014-10-12 17:49:59 +0200

    added rune colors

--- src/gtkfieldcard.c
+++ src/gtkfieldcard.c
@@ -6,32 +6,32 @@ GtkFieldCard*
 gtk_field_card_new_empty (GtkToggleButton* button, guint row, guint col)
 {
 	guint i;
-	
+
 	if (!button)
 		return NULL;
-	
+
 	GtkFieldCard* new = (GtkFieldCard*) calloc (1, sizeof(GtkFieldCard));
 
 	new->row = row;
 	new->col = col;
 	new->gcard = gtk_card_new_empty (button);
-	
+
 	for (i=0; i<4; i++)
 		new->elements[i] = rand() % ELEMENTS_NUM;
-	
+
 	for (i=0; i<4; i++)
 		switch (new->elements[i]){
 			case ET_BLUE:
-	    		gtk_widget_set_name (GTK_WIDGET (new->gcard->labels[i]), "movetellerplayer");	// ET_BLUE
+	    		gtk_widget_set_name (GTK_WIDGET (new->gcard->labels[i]), "bluerune");	// ET_BLUE
 				break;
 			case ET_GREEN:
-	    		gtk_widget_set_name (GTK_WIDGET (new->gcard->labels[i]), "");	// ET_GREEN
+	    		gtk_widget_set_name (GTK_WIDGET (new->gcard->labels[i]), "greenrune");	// ET_GREEN
 				break;
 			case ET_YELLOW:
-	    		gtk_widget_set_name (GTK_WIDGET (new->gcard->labels[i]), "");	// ET_YELLOW
+	    		gtk_widget_set_name (GTK_WIDGET (new->gcard->labels[i]), "yellowrune");	// ET_YELLOW
 				break;
 			case ET_RED:
-	    		gtk_widget_set_name (GTK_WIDGET (new->gcard->labels[i]), "movetellercpu");	// ET_RED
+	    		gtk_widget_set_name (GTK_WIDGET (new->gcard->labels[i]), "redrune");	// ET_RED
 				break;
 		}
 
@@ -39,12 +39,12 @@ gtk_field_card_new_empty (GtkToggleButton* button, guint row, guint col)
 	return new;
 }
 
-GtkFieldCard* 
+GtkFieldCard*
 gtk_field_card_new_with_values (GtkToggleButton* button, guint top, guint down, guint left, guint right, guint element, gboolean show, guint row, guint col)
 {
 	if (!button)
 		return NULL;
-	
+
 	GtkFieldCard* new = (GtkFieldCard*) calloc (1, sizeof(GtkFieldCard));
 
 	new->row = row;
@@ -59,7 +59,7 @@ gtk_field_card_new_random (GtkToggleButton* button, guint max, gboolean show, gu
 {
 	if (!button)
 		return NULL;
-	
+
 	GtkFieldCard* new = (GtkFieldCard*) calloc (1, sizeof(GtkFieldCard));
 
 	new->row = row;
@@ -74,7 +74,7 @@ gtk_field_card_get_gtk_card (GtkFieldCard* gfcard)
 {
 	if (!gfcard)
 		return NULL;
-	
+
 	return gfcard->gcard;
 }
 
@@ -83,7 +83,7 @@ gtk_field_card_get_row (GtkFieldCard* gfcard)
 {
 	if (!gfcard)
 		return -1;
-	
+
 	return gfcard->row;
 }
 
@@ -92,7 +92,7 @@ gtk_field_card_get_col (GtkFieldCard* gfcard)
 {
 	if (!gfcard)
 		return -1;
-	
+
 	return gfcard->col;
 }
 
@@ -108,7 +108,7 @@ gtk_field_card_clear (GtkFieldCard* gfcard)
 
 	for (i=0; i<4; i++)
 		gfcard->elements[i] = rand() % ELEMENTS_NUM;
-	
+
 	for (i=0; i<4; i++)
 		switch (gfcard->elements[i]){
 			case ET_BLUE:
@@ -134,8 +134,7 @@ gtk_field_card_free (GtkFieldCard* gfcard)
 {
 	if (!gfcard)
 		return ;
-	
+
 	gtk_card_free (gfcard->gcard);
 	free (gfcard);
 }
-
